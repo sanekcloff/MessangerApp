f4cc83d519c86726995fceeaec2ee91e6d46e520
sanekcl <sanekcl.sanek@gmail.com>
2024-10-11 22:00:42 +0300

    11/10/2024

--- MessangerApp/ApplicationData/Models/User.cs
+++ MessangerApp/ApplicationData/Models/User.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ApplicationData.Core.Context;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,15 +15,17 @@ namespace ApplicationData.Models
         public User() { }
         public User(byte[] image, string nickname, string email, string login, string password)
         {
+            var random = new Random();
+
             Id = Guid.NewGuid();
             Image = image;
-            Color = string.Empty;
+            Color = GenerateHexColor(random);
             Nickname = nickname;
-            Tag = string.Empty;
+            Tag = GenerateTag(random, nickname);
             CustomStatus = null;
             Email = email;
             Login = login;
-            PasswordHash = password;
+            PasswordHash = HashPassword(password);
             CreationDate = DateTime.Now;
             LastActive = DateTime.Now;
             Status = Statuses.Offline;
@@ -52,6 +56,51 @@ namespace ApplicationData.Models
         public string CreationDateFormated => CreationDate.ToString("f");
         public string LastActiveFormated => CreationDate.ToString("f");
 
+        string GenerateTag(Random random,string nickname)
+        {
+            var tag = string.Empty;
+            while (true)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    tag += random.Next(0,10);
+                    using (AppDbContext ctx = new())
+                    {
+                        var isExist = ctx.Users.Any(u => u.Nickname == nickname && u.Tag == tag);
+                        if (!isExist)
+                            return tag;                            
+                    }
+                }
+            }
+        }
+        string GenerateHexColor(Random random)
+        {
+            // Генерируем три случайных числа от 0 до 255 для RGB
+            int r = random.Next(256);
+            int g = random.Next(256);
+            int b = random.Next(256);
+
+            // Преобразуем в формат HEX
+            return $"#{r:X2}{g:X2}{b:X2}";
+        }
+        string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] saltedPassword = Encoding.UTF8.GetBytes(GenerateSalt() + password);
+                byte[] hashedPassword = sha256.ComputeHash(saltedPassword);
+                return Convert.ToBase64String(hashedPassword);
+            }
+        }
+        string GenerateSalt()
+        {
+            byte[] salt = new byte[16];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            return Convert.ToBase64String(salt);
+        }
     }
     public enum Statuses
     {
