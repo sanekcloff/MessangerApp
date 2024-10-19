3483f5462cc1f5bd8d44366c61b4fcd1804062f6
sanekcl <91375266+sanekcloff@users.noreply.github.com>
2024-10-19 12:50:53 +0300

    Style

---
+++ MessangerApp/ApplicationData/Utilities/ViewModel/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationData.Utilities.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected bool Set<T>(ref T field, T value, string propertyName)
+        {
+            if(EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        private void OnPropertyChanged(string propertyName)
+        { 
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- MessangerApp/Messanger/ViewModels/AuthorizationViewModel.cs
+++ MessangerApp/Messanger/ViewModels/AuthorizationViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using ApplicationData.Core.Context;
 using ApplicationData.Models;
 using ApplicationData.Services;
+using ApplicationData.Utilities.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -10,46 +12,8 @@ using System.Threading.Tasks;
 
 namespace Messanger.ViewModels
 {
-    public class AuthorizationViewModel
+    public class AuthorizationViewModel : ViewModelBase
     {
-        public AuthorizationViewModel(AppDbContext context)
-        {
-            this.context = context;
-            CurrentUser = new();
-        }
-        // Поле для хранения контекста
-        private AppDbContext context;
-        // Свойство для работы с User
-        public User CurrentUser { get; set; }
-        // Метод регистрации нового пользователя
-        public void RegistrateUser()
-        {
-            Debug.WriteLine(UserService.Create(CurrentUser, context));
-        }
-    }
-
-    public class RegisterationViewModel
-    {
-        public RegisterationViewModel(AppDbContext context)
-        {
-            this.context = context;
-        }
-
-        private AppDbContext context;
-
-        public User CurrentUser;
-
-        public void RegistrateUser()
-        {
-            Debug.WriteLine(UserService.Create(CurrentUser, context));
-        }
-    }
-    public class LoginViewModel
-    {
-        public LoginViewModel(AppDbContext context)
-        {
-            this.context = context;
-        }
-        private AppDbContext context;
+       
     }
 }
